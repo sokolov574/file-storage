f14b0ed4c5e5ba5162e5d2bc511e9075320ed0c2
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-15 21:23:30 +0100

    added signedout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
+import { SignIn, SignInButton, SignOutButton, SignedIn, SignedOut, useSession } from "@clerk/nextjs";
 
 export default function Home() {
+
+  const session = useSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignInButton mode="modal" />
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+      
+      
+      
       <Button>Hello World</Button>
     </main>
   );
