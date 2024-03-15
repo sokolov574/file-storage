f34251dfa1898ee2d97d9091ee8d822962a41952
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-15 21:12:58 +0100

    added sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignInButton mode="modal" />
       <Button>Hello World</Button>
     </main>
   );
