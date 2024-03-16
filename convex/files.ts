28a4e51feccc59a0f7eadb2c9c6643669cac6f7e
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-16 12:13:30 +0100

    added new button

---
+++ convex/files.ts
@@ -0,0 +1,12 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const createFile = mutation({
+    args: {
+        name: v.string(),
+    },
+    async handler (ctx, args) {
+        await ctx.db.insert("files", { 
+          name: args.name })
+    },
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,33 @@
 
 import { Button } from "@/components/ui/button";
 import { SignIn, SignInButton, SignOutButton, SignedIn, SignedOut, useSession } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+
 
 export default function Home() {
 
-  const session = useSession();
+  const createFile = useMutation(api.files.createFile)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedIn>
-        <SignOutButton />
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
       </SignedIn>
       <SignedOut>
-        <SignInButton mode="modal" />
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
       </SignedOut>
-      
-      
-      
-      <Button>Hello World</Button>
+
+      <Button onClick={() => {
+        createFile({
+          name: "Hello World"
+        })
+      }}>Click Me</Button>
     </main>
   );
 }
