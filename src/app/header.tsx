1edaa4587fd1732c44d1cee48d97beeba3760727
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-17 20:57:11 +0100

    added Organization Switcher

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,9 +1,11 @@
-import { UserButton, UserProfile } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton, UserProfile } from "@clerk/nextjs";
 
 export function Header() {
-    return <div className="border-b">
-        <div className="container mx-auto justify-between flex">
-            HELLO WORLD
+    return <div className="border-b py-4 bg-gray-50">
+        <div className="items center container mx-auto justify-between flex">
+            <div>FileStorage</div>
+            <div className="flex gap-2"/>
+            <OrganizationSwitcher />
             <UserButton />
         </div>
     </div>
