f8cc05e0bbf5c81c5e495929a580d9351468227b
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-17 20:13:06 +0100

    error with upload the files

--- convex/files.ts
+++ convex/files.ts
@@ -1,4 +1,4 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const createFile = mutation({
@@ -6,8 +6,15 @@ export const createFile = mutation({
         name: v.string(),
     },
     async handler (ctx, args) {
+      const identity = await ctx.auth.getUserIdentity();
+      
+      if (!identity) {
+        throw new ConvexError("You must be signed in to create a file")
+      }
+
         await ctx.db.insert("files", { 
-          name: args.name })
+          name: args.name,
+         })
     },
 })
 
