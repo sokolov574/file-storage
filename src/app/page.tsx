07d8dcf39ef4f26e9769788a1c4ff361756bbdbf
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-18 15:43:21 +0100

    fixed bags with organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,37 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
-import { SignIn, SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization, useSession } from "@clerk/nextjs";
+import { SignIn, SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization, useSession, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import clsx from "clsx";
 
 
 
 export default function Home() {
-  const { organization } = useOrganization()
-  const files = useQuery(api.files.getFiles,
-    organization?.id ? { orgId: organization.id } : "skip"
-  )
+  const  organization = useOrganization();
+  const user = useUser();
+
+  let orgId: string | undefined = undefined;
+  if(organization?.isLoaded && user?.isLoaded) {
+  orgId = organization?.organization?.id ?? user?.user?.id;
+}
+
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignedIn>
-        <SignOutButton>
-          <Button>Sign Out</Button>
-        </SignOutButton>
-      </SignedIn>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
 
       {files?.map(file => {
         return <div key={file._id}>{file.name}</div>
       })}
 
       <Button onClick={() => {
-        if(!organization) return
+        if (!orgId) return
         createFile({
           name: "Hello World",
-          orgId: organization.id,
+          orgId,
         })
       }}>Click Me</Button>
     </main>
