f7f438f3c599b5a370b57ba546ca83cce0eac92c
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-21 20:08:43 +0100

    fixed bag with ConvexError

--- convex/users.ts
+++ convex/users.ts
@@ -4,12 +4,19 @@ import { QueryCtx, internalMutation, MutationCtx } from "./_generated/server";
 export async function getUser(ctx: QueryCtx | MutationCtx, tokenIdentifier: string) 
 
 {
-    return await ctx.db
+    const user = await ctx.db
             .query("users")
             .withIndex('by_tokenIdentifier', q => 
                 q.eq('tokenIdentifier', tokenIdentifier)
             )
             .first();
+
+            if(!user) {
+                throw new ConvexError('User not found');
+            }
+
+
+            return user;
 }
 
 export const createUser = internalMutation({
@@ -27,10 +34,6 @@ export const addOrgIdToUser = internalMutation({
     args: { tokenIdentifier: v.string(), clerkId: v.string(), orgId: v.string() },
     async handler(ctx, args) {
         const user = await getUser(ctx, args.tokenIdentifier)
-
-        if(!user) {
-            throw new ConvexError('User not found');
-        }
 
         await ctx.db.patch(user._id, {
             orgIds: [...user.orgIds, args.orgId],
