cd75ce1efec93d43abfe5dc27e25b4a30bbae1b3
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-23 13:51:27 +0100

    Added form and applied styles to it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,51 @@
 'use client'
 
-import { Button } from "@/components/ui/button";
 import { SignIn, SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization, useSession, useUser } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import clsx from "clsx";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
+import { z } from "zod"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+ 
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z.any(),
+})
 
 
 export default function Home() {
   const  organization = useOrganization();
   const user = useUser();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
 
   let orgId: string | undefined = undefined;
   if(organization?.isLoaded && user?.isLoaded) {
@@ -25,13 +60,58 @@ export default function Home() {
   <div className="flex justify-between items-center">
     <h1 className="text-4xl font-bold">Your Files</h1>
 
-    <Button onClick={() => {
+    <Dialog>
+      <DialogTrigger asChild>
+      <Button onClick={() => {
       if (!orgId) return
       createFile({
         name: "Hello World",
         orgId,
       })
-    }}>Click Me</Button>
+    }}>Upload File</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="mb-4">Upload your File Here</DialogTitle>
+          <DialogDescription>
+          <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>File</FormLabel>
+              <FormControl>
+                <Input
+                type="file"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+   </Dialog>
+
   </div>
 
   {files?.map(file => {
