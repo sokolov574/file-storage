6ba0cacde199d1b7904aaa35daf0522719069ce2
sokolov574 <sokolovloveoleg@gmail.com>
2024-03-24 20:34:32 +0100

    edit isSubmitet to se isSubmitting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
+import { Loader2 } from "lucide-react";
  
 const formSchema = z.object({
   title: z.string().min(1).max(200),
@@ -150,9 +151,12 @@ return (
                     </FormItem>
                   )}
                 />
-                <Button type="submit"
-                disabled={form.formState.isLoading}
-                >Submit</Button>
+                <Button 
+                type="submit"
+                disabled={form.formState.isSubmitting} 
+                className="flex gap-1">
+                {form.formState.isSubmitting && <Loader2 className="h-4 w-4 animate-spin" />}
+                Submit</Button>
               </form>
             </Form>
           </DialogDescription>
