82602c225b551a973a7c6e7a433b4aae43b107bf
sokolov574 <sokolovloveoleg@gmail.com>
2024-04-02 16:28:27 +0200

    implement a new routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { api } from "../../convex/_generated/api";
 import { UploadButton } from "./upload-button";
 import { FileCard } from "./file-card";
 import Image from "next/image";
-import { FileIcon, Loader2 } from "lucide-react";
+import { FileIcon, Loader2, StarIcon } from "lucide-react";
 import { SearchBar } from "./search-bar";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
@@ -47,14 +47,19 @@ const isLoading = files === undefined;
 
 return (
   <main className="container mx-auto pt-12">
-    <div className="flex gap-10">
-    <div className="w-40 flex justify-start">
-      <Link href="/">
+    <div className="flex gap-8">
+    <div className="w-40 flex flex-col gap-4">
+      <Link href="/dashboard/files">
         <Button variant={"link"} className="flex gap-2">
           <FileIcon /> All Files
         </Button>
       </Link>
 
+      <Link href="/dashboard/favorites">
+        <Button variant={"link"} className="flex gap-2">
+          <StarIcon /> Favorites
+        </Button>
+      </Link>
 
       
     </div>
