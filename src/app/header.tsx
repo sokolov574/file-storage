b85cdb286c758f2abc9eb184d0bebfcb27de2ee9
sokolov574 <sokolovloveoleg@gmail.com>
2024-04-17 15:27:05 +0200

    added a small logo for the site name

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, SignInButton, SignedOut, UserButton, UserProfile } from "@clerk/nextjs";
+import Image from "next/image";
 
 export function Header() {
     return <div className="border-b py-4 bg-gray-50">
              <div className="items center container mx-auto justify-between flex">
-                <div>FileStorage</div>
+                <div>
+                    <Image src="/logo.png" width="80" height="80" alt="file storage logo"/>
+                 FileStorage</div>
                   <div className="flex gap-2"/>
             <OrganizationSwitcher />
             <UserButton />
