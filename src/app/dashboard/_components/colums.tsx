7ff99a26a2b124ef2a86ca8d15987bbb31baf2bc
sokolov574 <sokolovloveoleg@gmail.com>
2024-04-18 17:41:13 +0200

    make a user avatar to the colums

--- src/app/dashboard/_components/colums.tsx
+++ src/app/dashboard/_components/colums.tsx
@@ -5,13 +5,22 @@ import { Doc, Id } from "../../../../convex/_generated/dataModel"
 import { formatRelative } from "date-fns"
 import { useQuery } from "convex/react"
 import { api } from "../../../../convex/_generated/api"
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
 
 function UserCell({ userId }: { userId: Id<"users"> }) {
     const userProfile = useQuery(api.users.getUserProfile, {
         userId: userId,
     })
 
-    return<div>{userProfile?.name}</div>
+    return (
+        <div className="flex gap-2 text-xs text-gray-700 w-40 items-center">
+                <Avatar className="w-6 h-6">
+                  <AvatarImage src={userProfile?.image} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            {userProfile?.name}
+        </div>
+    ) 
   }
 
 export const columns: ColumnDef<Doc<"files">>[] = [
