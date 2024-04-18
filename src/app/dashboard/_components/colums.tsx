02f6e8567948c5e2e5f1a135717eadd740bb38fa
sokolov574 <sokolovloveoleg@gmail.com>
2024-04-18 17:34:11 +0200

    added a function user cell

--- src/app/dashboard/_components/colums.tsx
+++ src/app/dashboard/_components/colums.tsx
@@ -1,9 +1,18 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { Doc } from "../../../../convex/_generated/dataModel"
+import { Doc, Id } from "../../../../convex/_generated/dataModel"
 import { formatRelative } from "date-fns"
+import { useQuery } from "convex/react"
+import { api } from "../../../../convex/_generated/api"
 
+function UserCell({ userId }: { userId: Id<"users"> }) {
+    const userProfile = useQuery(api.users.getUserProfile, {
+        userId: userId,
+    })
+
+    return<div>{userProfile?.name}</div>
+  }
 
 export const columns: ColumnDef<Doc<"files">>[] = [
   {
@@ -14,11 +23,18 @@ export const columns: ColumnDef<Doc<"files">>[] = [
     accessorKey: "type",
     header: "Type",
   },
+
+  {
+    header: "User",
+    cell: ({ row }) => {
+   
+        return <UserCell userId={row.original.userId}/>;
+      },
+  },
+
   {
     header: "Upload On",
     cell: ({ row }) => {
-        const amount = parseFloat(row.getValue("createdOn"))
-   
         return (
         <div>
             {formatRelative(new Date(row.original._creationTime), new Date())}</div>
