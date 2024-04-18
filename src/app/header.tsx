42a2bdd609271b30c584a310e89fd04607bdf971
sokolov574 <sokolovloveoleg@gmail.com>
2024-04-18 08:43:11 +0200

    added some style for a logo adn make logo as link

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, SignInButton, SignedOut, UserButton, UserProfile } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Header() {
     return <div className="border-b py-4 bg-gray-50">
              <div className="items center container mx-auto justify-between flex">
-                <div>
-                    <Image src="/logo.png" width="80" height="80" alt="file storage logo"/>
-                 FileStorage</div>
+                <Link href="/" className="flex gap-2 items-center text-xl">
+                    <Image src="/logo1.png" width="40" height="40" alt="file storage logo"/>
+                 FileStorage</Link>
                   <div className="flex gap-2"/>
             <OrganizationSwitcher />
             <UserButton />
