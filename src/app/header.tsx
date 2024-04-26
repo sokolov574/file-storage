0a9c9b133afaa69078f41dde6ea1e498cdcdf012
sokolov574 <sokolovloveoleg@gmail.com>
2024-04-26 14:08:42 +0200

    adding landing page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,19 +1,21 @@
 import { Button } from "@/components/ui/button";
-import { OrganizationSwitcher, SignInButton, SignedOut, UserButton, UserProfile } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignInButton, SignedIn, SignedOut, UserButton, UserProfile, useSession } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
-    return <div className="border-b py-4 bg-gray-50">
+
+    return (
+    <div className="relative z-10 border-b py-4 bg-gray-50">
              <div className="items center container mx-auto justify-between flex ">
                 <Link href="/" className="flex gap-2 items-center text-xl text-black">
                     <Image src="/logo1.png" width="40" height="40" alt="file storage logo"/>
                  FileStorage</Link>
                  
                 <Button className="text-black" variant={"outline"} >
-                  <Link href="/dashboard/files">Go to Your Files</Link>  
+                  <Link href="/dashboard/files">Go to Your Files</Link> 
                 </Button>    
-
+                  <SignedIn />
                   <div className="flex gap-2"/>
             <OrganizationSwitcher />
             <UserButton />
@@ -27,7 +29,7 @@ export function Header() {
             
         </div>
     </div>
-}
+)}
 
 // This is the Header component. It includes the OrganizationSwitcher and UserButton components.
 // When the user is signed out, the SignInButton is displayed.
