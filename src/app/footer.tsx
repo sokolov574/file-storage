e830447b1f763610b686fbd2b506c5fc4710bad7
sokolov574 <sokolovloveoleg@gmail.com>
2024-04-26 13:32:50 +0200

    footer is done

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,5 +1,24 @@
-export function Footer() {
-    return <div className="h-40 bg-gray-100">
+import Link from "next/link";
 
+export function Footer() {
+  return (
+    <div className="h-40 bg-gray-100 mt-12 flex items-center">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>FileStorage</div>
+
+        <Link className="text-blue-900 hover:text-blue-500" href="/privacy">
+          Privacy Policy
+        </Link>
+        <Link
+          className="text-blue-900 hover:text-blue-500"
+          href="/terms-of-service"
+        >
+          Terms of Service
+        </Link>
+        <Link className="text-blue-900 hover:text-blue-500" href="/about">
+          About
+        </Link>
+      </div>
     </div>
+  );
 }
